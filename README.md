8d5b05a2a46d0a707a0d7b29ac47faf7e735c002
BryantLan12138 <bryant.lan1212@gmail.com>
2020-11-12 22:16:33 +1100

    finished the project

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ A simple web application based on Theater scenario, used by general HTML CSS and
 
 <p>User are allowed to select any seat which is not occupied, the simple application could sum the total price for user, based on the number of selected seats and movie.
 </p>
+</br>
+<p>Also, this application has capabilities to store the user's decesion on localStorage, after refresh the browser the selected data could be represented automatically with the updated numbers of seats and total price.
+</p>
--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 const container = document.querySelector('.container');
-const seats = document.querySelectorAll('row .seat:not(.Occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.Occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
 let ticketPrice = parseInt(movieSelect.value);
 
+populateUI();
+
 //Update total count of selected seats 
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.Selected');
+
+    //store the index of selected seats
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+    
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
     const selectedSeatsCount = selectedSeats.length;
-    // console.log(selectedSeatsCount);
+
+    //update HTML element
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
+    
+}
+
+//save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice)
 }
 
 //event listener on seats 
@@ -20,13 +36,37 @@ container.addEventListener('click', e => {
     if(e.target.classList.contains('seat') && !e.target.classList.contains('Occupied')) {
         // console.log(e.target);
         e.target.classList.toggle('Selected');
-
         updateSelectedCount();
     }
 });
 
+//Get data from localStorage and popolate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    
+    if(selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('Selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if(selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
+
+
 //Movie select event
 movieSelect.addEventListener('change', e => {
     ticketPrice = parseInt(e.target.value);
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
-})
+});
+
+//Initial count and total set
+
+updateSelectedCount();
